088e41cfd64df878e9f3d27cacdac5439b081248
drummatwit <drumm@wit.edu>
2025-10-10 13:01:41 -0400

    Completed array_create

--- array_create.c
+++ array_create.c
@@ -1,9 +1,31 @@
 #include <stddef.h> // For NULL
-// TODO: Include any necessary header files here
+#include <stdlib.h>
+
 
 /**
- * TODO: Describe what the function does
+ * Creates and retuens a dynamically allocated array of 
+ * even integers from begin to end inclusive
  */
 int* array_create_evens(int begin, int end) {
-    return NULL;
+    if (begin > end){
+        return NULL;
+    }
+    if (begin % 2 != 0){
+        begin++;
+    }
+    if (end % 2 != 0){
+        end--;
+    }
+    if (begin > end){
+        return NULL;
+    }
+    int count = ((end - begin) / 2) + 1;
+    int* evens = malloc(count * sizeof(int));
+    if (evens == NULL){
+        return NULL;
+    }
+    for (int i = 0; i < count; i++){
+        evens[i] = begin + (i *2);
+    }
+    return evens; 
 }
