abb0ccb385a5db7f673b33577a220cc71312f699
drummatwit <drumm@wit.edu>
2025-10-10 13:11:24 -0400

    Completed string_fill

--- string_fill.c
+++ string_fill.c
@@ -1,7 +1,23 @@
 #include <stddef.h> // For NULL
 
 /**
- * TODO: Describe what the function does
+ * Fills a character array with character c
+ * It leaves space for null terminator at the end. 
  */
 void string_fill(char dest[], int dest_cap, char c) {
+    if (dest == NULL ){
+        return;
+    }
+    if (dest_cap == 0){
+
+    } else if (dest_cap == 1){
+        dest[0] = '\0';
+        return;
+    } else {
+        for (int i= 0; i < dest_cap - 1; i++){
+            dest[i] = c;
+        }
+        dest[dest_cap - 1] = '\0'; 
+    }
+
 }
