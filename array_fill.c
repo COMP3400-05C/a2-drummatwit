d98505f089700488a17d94bce746b94e6a8c8a5b
drummatwit <drumm@wit.edu>
2025-10-10 13:09:56 -0400

    Completed array_fill

--- array_fill.c
+++ array_fill.c
@@ -4,5 +4,16 @@
  * TODO: Describe what the function does
  */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    if (array == NULL || array_len <= 0){
+        return 0;
+    }
+    int count = 0;
+    for (int i = begin; i <= end; i++){
+        if (count == array_len){
+            break;
+        } 
+        array[count] = i;
+        count++;
+    }
+    return count;
 }
